b1e6470cff6e57d55bffd5d6442af05c54237d60
IGulaker <ig.pro@hotmail.com>
2021-11-01 13:26:33 +0100

    Initial Commit.
    The first commit of the group project.

---
+++ Grupp_5_Garage_v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grupp_5_Garage_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
