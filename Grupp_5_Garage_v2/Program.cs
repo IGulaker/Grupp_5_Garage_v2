d7111922cad14f07e835cf116a621dea3aed99f9
andreassundberg83 <ANDREASSUNDBERG83@HOTMAIL.COM>
2021-11-02 10:09:12 +0100

    Test

--- Grupp_5_Garage_v2/Program.cs
+++ Grupp_5_Garage_v2/Program.cs
@@ -6,7 +6,7 @@ namespace Grupp_5_Garage_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Grupp 5!");
         }
     }
 }
