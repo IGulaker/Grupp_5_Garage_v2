16a84390dc7dcdb5c867608e9c146a6235c049d0
Marie Sundberg <mariiesundberg@hotmail.com>
2021-11-02 10:50:39 +0100

    Gjort klassen Garage

---
+++ Grupp_5_Garage_v2/Garage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_5_Garage_v2
+{
+    class Garage
+    {
+        public int Number { get; set; }
+    }
+}
