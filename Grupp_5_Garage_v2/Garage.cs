bbbb6e801bcc472763693a26a395066c1394f84a
nesim83sm <nesim83sm@yahoo.com>
2021-11-02 20:29:14 +0100

    modify class Garage

--- Grupp_5_Garage_v2/Garage.cs
+++ Grupp_5_Garage_v2/Garage.cs
@@ -8,6 +8,73 @@ namespace Grupp_5_Garage_v2
 {
     class Garage
     {
-        public int Number { get; set; }
+        public int NumberOfParkingLots
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<Vehicle> ParkedVehicles
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<Vehicle> UnparkedVehicle
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void ListVehicles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ListTypeOfVehicle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddVehicle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveVehicle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vehicle SearchVehicle(string regNum)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string SearchByColor()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SearchByModelYear(int model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SearchByManufacturer(string manufacturer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SearchByReceiptNumber(int receiptNumber)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
