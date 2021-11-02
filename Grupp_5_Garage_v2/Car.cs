3cf6eb820d9037cd1899a89741e0384303d332d7
nesim83sm <nesim83sm@yahoo.com>
2021-11-02 20:18:57 +0100

    added class Garage and Car

---
+++ Grupp_5_Garage_v2/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_5_Garage_v2
+{
+    internal class Car
+    {
+        public Car()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Car(bool rails, int numofdoors)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Rails
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int NumberOfDoors
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary></summary>
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
