b4dcb5a25abced4f10d2522c2677daa20aa9d374
info@viktorstahl.com <info@viktorstahl.com>
2021-11-02 10:22:11 +0100

    Test

---
+++ Grupp_5_Garage_v2/GarageManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_5_Garage_v2
+{
+    public class GarageManager
+    {
+        public int ThisIsAnInt { get; set; }
+    }
+}
